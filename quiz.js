dcbba287df81f15d5d85e619c2dc7c7fb7fdedb9
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-01 14:58:02 -0600

    wrote logic in english with basic geography

--- quiz.js
+++ quiz.js
@@ -1,1 +1,19 @@
 // Create a `tree` function
+function tree (objectNAME) {
+
+
+
+}
+
+// objectNAME {
+
+//  height: "put a getter for id of howTall text input"
+//   whichkey: "addeventlistener to whichChar textarea. then capture by keycode, key etc.."
+
+// 1. A key that specifies the height of the pine tree.
+// 1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
+// 1. A key that specifies which character to use to build the pine tree.
+// 1. The character to use should be from user input in a `<input type="text">` field in the DOM.
+//
+//
+ }
