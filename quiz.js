3b32b75f160d136e904a5c0630d4849c333625f0
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-01 15:51:50 -0600

    this math does not work in javascript/ looking at string methods

--- quiz.js
+++ quiz.js
@@ -1,19 +1,39 @@
 // Create a `tree` function
-function tree (objectNAME) {
+function tree() {
+
+  // firstLineSpace = trunk.height;
+  // secondLineSpace = firstLineSpace - 1
+  //  trunk.height
+  // trunk.whichKey  string methods
+  //
+  console.log((" " * trunk.height) + trunk.whichKey)
+
+
+
+
+
+
+
+
     // access object properites and do the stuff to build the tree
     // print character (objectName.whichKey) to console.log
     // go to next line (find the center???) print amount from previous line + 1  REPEAT
 
     // loop contingent on height property from user input  .length
+    // start point contingent on  height as well
+    //
+    // how is the center determined?  line one = n
+    //                                line two += n+2
+    //                                line three= n + 2
 
 
 
 }
 
-// objectNAME {
+var  trunk = {
 
-//  height: "put a getter for id of howTall text input"
-//   whichkey: "addeventlistener to whichChar textarea. then capture by keycode, key etc.."
+ height: 7,      // "put a getter for id of howTall text input"
+ whichKey: "*"        //addeventlistener to whichChar textarea. then capture by keycode, key etc..
 
 // 1. A key that specifies the height of the pine tree.
 // 1. The value for the height of the tree should be from user input in a `<input type="text">` field in the DOM.
