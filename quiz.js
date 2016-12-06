50a502350d6c10e740144d0a9a31f96e7e44572a
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-06 14:47:22 -0600

    created function for enter key/ duplicated and assigned to each text field

--- quiz.js
+++ quiz.js
@@ -9,40 +9,52 @@ var active = document.activeElement;
 // ----Submit button action---------------
 
 
-var runFunction = document.querySelector("#growButton").addEventListener('click', function() {
+function hereIStheAlert (e) {
 
         if( theHeight.value == "" || theChar.value == ""){
 
           alert("Both fields must have a value");
 
+          }
+
+        else {
+
+          return tree(trunk)
         }
 
-      else {
-
-      return tree(trunk)
-  }
-
-   })
-
-// var focusFIELDforHeight =  document.querySelector("heightId")
-// var focusFIELDforsymbol =  document.querySelector("characterId")
-
-// if(focusFIELDforHeight === true || focusFIELDforsymbol === true
+}
 
 
 
-//   .addEventListener('keypress', function(enter) {
-
-//   if(enter.keyCode === 13 ) {
-
-//     // console.log("you hit the enter button")
+ document.querySelector("#growButton").addEventListener('click', hereIStheAlert)
 
 
-//     return tree(trunk)
 
-//   }
+var focusFIELDforHeight =  document.getElementById("heightId")
+var focusFIELDforsymbol =  document.getElementById("characterId")
 
-// })
+
+
+  focusFIELDforHeight.addEventListener('keypress', function(e) {
+
+            if(e.keyCode === 13 ) {
+
+               hereIStheAlert(e)
+
+            }
+
+})
+
+
+  focusFIELDforsymbol.addEventListener('keypress', function(e) {
+
+            if(e.keyCode === 13 ) {
+
+               hereIStheAlert()
+
+            }
+
+})
 
 
 // Object----------------------------------
@@ -68,54 +80,3 @@ function tree() {
 
   }
 }
-
-
-
-
-
-
-// option 1-keypress events and check whether it was enter BUT NOT FROM 1996  .key
-// option 2-listen for submt events
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// example of setting html to result of javscipt
-
-// document.getElementById("holdingResult").innerHTML = (inputTemp - 32) * 5/9
-//
-//
-//
-
-// //   hitting enter to activate  function
-// //    document.addEventListener('keypress', function(enter) {
-
-//     // console.log(enter)
-//     if (enter.keyCode === 13) {
-
-
-//           var inputTemp = document.getElementById("temptemp").value
-//               // console.log(typeof inputTemp)
-
-//         if (document.querySelector('#toCelsius').checked === true ) {
-//           radioCelsius(inputTemp)
-//         }
-//         else {
-//           radioFahrenheit(inputTemp)
-
-//         }
-
-//     }
-//   }
-// )
